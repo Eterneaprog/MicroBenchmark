c556e8d30f96d3c5c8b46e9e5b2789dd3dd7ad8c
Eternea <77746200+Eterneaprog@users.noreply.github.com>
2021-03-10 23:16:51 +0100

    Implémentation exos

---
+++ Scripts/Exercice2.R
@@ -0,0 +1,13 @@
+# Exercice 2 : Comparaison de deux fonctions simples
+
+# Consigne ----------------------------------------------------------------
+# Trouvez le moyen d'exprimer 3*2 de différentes manières 
+# et comparez les temps de calculs :
+
+# Votre code : ------------------------------------------------------------
+
+
+
+# Interpretation : --------------------------------------------------------
+
+# Quelle fonction est la plus rapide ? 
---
+++ Scripts/Exercice4.R
@@ -0,0 +1,18 @@
+#Exercice 4 : Probabilités
+
+# Consigne ----------------------------------------------------------------
+# Comparer le temps d'éxécution de tirages aléatoires
+# de différentes lois de probabilités (loi de poisson, loi binomiale ... )
+
+  # Nous avons choisi n = 1000 mais il est possible de tester cette fonction
+  # avec n aussi grand que vous le souhaitez
+
+# Comparer ensuite les temps de création
+
+# Votre code : ------------------------------------------------------------
+
+
+
+# Interpretation : --------------------------------------------------------
+
+# Quelle est la loi la plus simple à générer pour R ?
